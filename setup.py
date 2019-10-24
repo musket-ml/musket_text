52d7d9525aab4ea39e27c9a3a1975acb643c4e5f
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-10-24 17:28:12 +0700

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='musket_text',
-      version='0.48',
+      version='0.43',
       description='Common parts of my pipelines',
       url='https://github.com/petrochenko-pavel-a/musket_core',
       author='Petrochenko Pavel',
@@ -10,9 +10,5 @@ setup(name='musket_text',
       packages=setuptools.find_packages(),
       include_package_data=True,
       dependency_links=['https://github.com/aleju/imgaug'],
-      install_requires=["numpy", "scipy","Pillow", "cython","pandas","matplotlib", "scikit-image","keras>=2.2.4","imageio",
-"opencv-python",
-"h5py",
-"tqdm",
-"segmentation_models", "lightgbm", "async_promises"],
+      install_requires=["musket_core"],
       zip_safe=False)
